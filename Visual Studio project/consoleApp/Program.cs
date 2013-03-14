9eb3bf6b8eac5c30767e0e49ce3081aa095c6ea0
Jiri Zikmund <jiri.zikmund@socialbakers.com>
2013-03-14 16:06:18 +0100

    ConsoleApp skeleton - base usage of library

---
+++ Visual Studio project/consoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarExpenses;
+
+namespace CarExpensesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Initializing application... ");
+                CoreApp app = CoreApp.init;
+                Console.WriteLine("OK");
+
+                Console.Write("Logging in... ");
+                Console.WriteLine(app.login("jirka", "heslo").message);
+
+                Console.ReadLine();
+            }
+            catch (CarExpensesException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
